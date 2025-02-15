5d125d07230f75d4fd70be734c9bcd01a968ab51
Jade Kam <jadekam2018@outlook.com>
2025-02-16 00:39:49 +1100

    Finished base functionalities

--- src/slash-create.js
+++ src/slash-create.js
@@ -1,61 +1,78 @@
-const {REST, Routes, SlashCommandBuilder} = require('discord.js')
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 require('dotenv').config();
 
-// Info needed for slash commands
-const botID = "1339216182544175166"
-const serverID = "1214916322111127552"
-const botToken = process.env.DISCORD_TOKEN
+const botID = "1339216182544175166";
+const serverID = "1214916322111127552";
+const botToken = process.env.DISCORD_TOKEN;
 
-const rest = new REST().setToken(botToken)
+const rest = new REST().setToken(botToken);
 
-// Register the commands
 const slashRegister = async () => {
   try {
-    // Register the commands
     await rest.put(
-      // endpoint for registering commands (where bot should register slash commands)
       Routes.applicationGuildCommands(botID, serverID),
-      // info request 
-      {body: [
-        new SlashCommandBuilder()
-        .setName("create")
-        .setDescription("Create a budget")
-        .addStringOption(option => {
-          return option 
-          .setName("name")
-          .setDescription("The name of the budget")
-          // required options must always be above non-required options
-          .setRequired(true)
-          .setMaxLength(30)
-        }),
+      {
+        body: [
+          new SlashCommandBuilder()
+            .setName("create")
+            .setDescription("Create a budget")
+            .addStringOption(option => 
+              option
+                .setName("name")
+                .setDescription("The name of the budget")
+                .setRequired(true)
+                .setMaxLength(30)
+            )
+            .addNumberOption(option =>
+              option
+                .setName("amount")
+                .setDescription("The total budget amount")
+                .setRequired(true)
+                .setMinValue(0)
+            ),
 
-        new SlashCommandBuilder() 
-        .setName("delete")
-        .setDescription("Delete a budget"),
+          new SlashCommandBuilder()
+            .setName("view")
+            .setDescription("View a budget")
+            .addStringOption(option =>
+              option
+                .setName("name")
+                .setDescription("The name of the budget")
+                .setRequired(true)
+            ),
 
-        new SlashCommandBuilder()
-        .setName("list")
-        .setDescription("List all budgets"),
+          new SlashCommandBuilder()
+            .setName("list")
+            .setDescription("List all budgets"),
 
-        new SlashCommandBuilder()
-        .setName("update")
-        .setDescription("Update a budget"),
-
-        new SlashCommandBuilder()
-        .setName("add")
-        .setDescription("Add an expense to a budget"),
-
-        new SlashCommandBuilder()
-        .setName("remove")
-        .setDescription("Remove an expense from a budget"),
-
-        new SlashCommandBuilder()
-        .setName("view")
-        .setDescription("View a budget")
-      ]}
-    )
+          new SlashCommandBuilder()
+            .setName("add")
+            .setDescription("Add an expense to a budget")
+            .addStringOption(option =>
+              option
+                .setName("budget")
+                .setDescription("The name of the budget")
+                .setRequired(true)
+            )
+            .addNumberOption(option =>
+              option
+                .setName("amount")
+                .setDescription("The expense amount")
+                .setRequired(true)
+                .setMinValue(0)
+            )
+            .addStringOption(option =>
+              option
+                .setName("description")
+                .setDescription("Description of the expense")
+                .setRequired(true)
+            )
+        ]
+      }
+    );
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
+
 slashRegister();
